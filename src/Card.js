adc727621ac459b69e551ffc33086eff779049f7
Roshan Elvitigala <roshanrox911@gmail.com>
2020-03-01 11:08:20 +1100

    created the card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div className = 'bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' src='https://robohash.org/test?200x200'/>
+			<div>
+				<h2>Roshan</h2>
+				<p>roshanrox911@gmail.com</p>
+			</div>
+		</div>
+		);
+}
+
+export default Card;
